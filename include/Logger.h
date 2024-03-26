e7a8ee2c5d7a79d9de168e1ffacb03801127b01d
delta-12 <btesq235@gmail.com>
2024-03-26 12:11:16 +0100

    add cmake

---
+++ include/Logger.h
@@ -0,0 +1,11 @@
+/**
+ * @file Logger.h
+ *
+ * @brief C logger.
+ *
+ ******************************************************************************/
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#endif
